ef75d8faecc7fc56f30310363087ef20bea3f518
Jessem Ettaghouti <jessemtaghouti.prepa@gmail.com>
2023-11-22 17:22:18 +0100

    Réalisation des animations et finir le côté formations. Tout va bien pour le moment

--- script.js
+++ script.js
@@ -1,23 +1,62 @@
 // Intersection observer
-
-
-const observer = new IntersectionObserver((entries)=>{
+const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting){
+        if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        }
-        else {
-            entry.target.classList.remove('show');
+            if (entry.target.classList.contains('slide-in')) {
+                entry.target.classList.add('show-slide-in');
+            }
+            if (entry.target.classList.contains('fade-in')) {
+                entry.target.classList.add('show-fade-in');
+            }
+            if (entry.target.classList.contains('typewriter')) {
+                entry.target.classList.add('typing-start');
+                
+            }
+        } else {
+            entry.target.classList.remove('show', 'show-slide-in', 'show-fade-in','typing-start');
         }
     });
 });
 
+const Elements = document.querySelectorAll('.hidden, .animated');
+Elements.forEach((el)=> observer.observe(el));
 
 
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el)=> observer.observe(el));
 
-const hiddenElementsContact = document.querySelectorAll('contact-info');
-hiddenElementsContact.forEach((el)=> observer.observe(el));
+
+//------------------------------------------------------------------------------------------------
+
+
+
+function typeWriter(element, segments, segmentIndex, charIndex, callback) {
+    if (segmentIndex < segments.length) {
+        if (charIndex < segments[segmentIndex].length) {
+            element.innerHTML += segments[segmentIndex].charAt(charIndex);
+            setTimeout(function() {
+                typeWriter(element, segments, segmentIndex, charIndex + 1, callback);
+            }, 30);
+        } else {
+            // Ajoutez un saut de ligne si ce n'est pas le dernier segment
+            if (segmentIndex < segments.length - 1) {
+                element.innerHTML += '<br>';
+            }
+            setTimeout(function() {
+                typeWriter(element, segments, segmentIndex + 1, 0, callback);
+            }, 100);
+        }
+    } else if (typeof callback == 'function') {
+        callback();
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var element = document.getElementById('typewriter');
+    var text = element.innerHTML;
+    var segments = text.split('<br>'); // Découpez le texte en segments
+    element.innerHTML = ''; // Effacez le contenu initial
+    typeWriter(element, segments, 0, 0, function() {
+        // Callback après la fin de l'animation
+    });
+});
