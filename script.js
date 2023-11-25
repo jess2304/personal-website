857a8b5777559080026c0956d4ce74c8e60e2f9b
Jessem Ettaghouti <jessemtaghouti.prepa@gmail.com>
2023-11-25 18:24:50 +0100

    add shadows to skills

--- script.js
+++ script.js
@@ -70,17 +70,17 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
 
     var isimaSubjects = [
-        "Langues et Communication (Anglais, Allemand, Expression et Communication)",
-        "Gestion et Management (Organisation des entreprises, Gestion de projet, Management RH)",
-        "Informatique Fondamentale (Algorithmique, Structures de données, Automates, Langage C, UNIX, Java, C++, UML)",
-        "Développement et Programmation (Programmation fonctionnelle, Programmation linéaire, IOT)",
-        "Systèmes et Réseaux (Architecture des processeurs, Systèmes d'exploitation, Réseaux)",
-        "Science des Données (Base de données, Apprentissage statistique, Analyse de données, Big Data, Apprentissage profond)",
-        "Calcul et Modélisation (Analyse numérique, Probabilités, Calcul différentiel, Modélisation mathématique, Optimisation, Python, MATLAB)",
-        "Ingénierie et Technologie (Physique, Traitement du signal, Mécanique du solide, Eléments finis, CAO)",
-        "Cybersécurité et Éthique (Sensibilisation à la cybersécurité, Éthique, déontologie)",
-        "Projets (Projets de 2ème année)",
-        "Stage et Alternance (2ème année et 3ème année)"
+        "- Langues et Communication (Anglais, Allemand, Expression et Communication)",
+        "- Gestion et Management (Organisation des entreprises, Gestion de projet, Management RH)",
+        "- Informatique Fondamentale (Algorithmique, Structures de données, Automates, Langage C, UNIX, Java, C++, UML)",
+        "- Développement et Programmation (Programmation fonctionnelle, Programmation linéaire, IOT)",
+        "- Systèmes et Réseaux (Architecture des processeurs, Systèmes d'exploitation, Réseaux)",
+        "- Science des Données (Base de données, Apprentissage statistique, Analyse de données, Big Data, Apprentissage profond)",
+        "- Calcul et Modélisation (Analyse numérique, Probabilités, Calcul différentiel, Modélisation mathématique, Optimisation, Python, MATLAB)",
+        "- Ingénierie et Technologie (Physique, Traitement du signal, Mécanique du solide, Eléments finis, CAO)",
+        "- Cybersécurité et Éthique (Sensibilisation à la cybersécurité, Éthique, déontologie)",
+        "- Projets (Projets de 2ème année)",
+        "- Stage et Alternance (2ème année et 3ème année)"
       ];
     var ipestSubjects = [
         "Mathématiques (Analyse et Algèbre)",
